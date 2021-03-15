da54339e6b9b2130ce302d60a1aa0311bff2f9bc
Islam <Isla_97@bk.ru>
2021-03-15 06:05:44 +0300

    delete All

--- serverside/interfce/AuthenticationService.java
+++
@@ -1,7 +0,0 @@
-package serverside.interfce;
-
-public interface AuthenticationService {
-    void start();
-    void stop();
-    String getNickByLoginAndPassword(String login, String password);
-}
